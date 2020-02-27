cac0aeb3f5ca414d626463df59025f87e8889588
joyce-paul <joyce_paul@hotmail.com>
2020-02-27 16:26:26 +0800

    Added input onChange form

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+    constructor(){
+      super()
+
+      this.state = {
+        word : ""
+      }
+    }
+
+
+changeHandler(event){
+  this.setState({word:event.target.value});
+  console.log("change", event.target.value);
+}
+
+
   render() {
     return (
       <div>
-        Welcome.
+        <h4>Welcome</h4>
+
+       <input onChange={(event)=>{this.changeHandler(event);}}/>
       </div>
     );
   }
